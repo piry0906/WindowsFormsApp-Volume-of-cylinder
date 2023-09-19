d06675a0660720b3eae0d49965a83ceeca8f17fb
Piroska Gilmore <piry0906@gmail.com>
2023-09-19 18:20:42 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_Volume_of_cylinder {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if (double.TryParse(textBox1.Text, out double Radius) && double.TryParse(textBox2.Text, out double Height)) {
+                const double pi = Math.PI;
+                double Volume = pi * Math.Pow(Radius, 2) * Height;
+                labelAnswer.Text = $"Volume of Cylinder is {Volume:N2}";
+            }
+            else {
+                MessageBox.Show("Invalid data. Please enter valid numbers in the TextBoxes.");
+            }
+    
+        }
+    }
+}
